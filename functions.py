3f464ffba20410b1a4bdf82877aeaa8c43fdac36
Antwaun Johnson <antwaun.l.johnson@gmail.com>
2020-07-05 22:05:19 -0700

    working with functions

---
+++ functions.py
@@ -0,0 +1,11 @@
+def returning_user():
+    returning_user = input("\nwhat is your name?")
+    print(f"\nHello {returning_user.title()}, we are glad to have you back!")
+
+returning_user()
+
+def new_user():
+    new_user = input("\nwhat is your name?")
+    print(f"\nHello {new_user.title()}, welcome to this awesome experience!")
+
+new_user()
